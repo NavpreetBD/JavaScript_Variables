55b770b8ea1059dc13efa978081f347999040d1b
navpreet <navpreetbatth2.nb@gmail.com>
2024-03-04 09:59:14 -0800

    initial commit

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 and 2
+const firstName = "Navpreet";
+let lastName = "Dhillon";
+var age = 29;
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+//to see results
+console.log(`${author}wrote ${title}.It is ${pageCount} long`);
+
